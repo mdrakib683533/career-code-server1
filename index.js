8c3bcfe583bc9d38b7384c533a54b350816b35ba
rakibhossen123 <osmangoni87367@gmail.com>
2026-09-20 01:42:17 +0600

    fix cors for firebase hosting

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ require("dotenv").config();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://career-code-client-20f2a.web.app",
+    ],
     credentials: true,
   }),
 );
