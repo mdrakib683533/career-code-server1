7f30fdffca3ed1002a89754a8b5cc58352f360dd
rakibhossen123 <osmangoni87367@gmail.com>
2026-09-19 22:46:58 +0600

    fix firebase app initialization

--- index.js
+++ index.js
@@ -19,16 +19,19 @@ app.use(express.json());
 app.use(cookieParser());
 
 // firebase admin
-const { initializeApp, cert } = require("firebase-admin/app");
+const { getApps, initializeApp, cert } = require("firebase-admin/app");
 const { getAuth } = require("firebase-admin/auth");
 
-initializeApp({
-  credential: cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  }),
-});
+const firebaseApp =
+  getApps().length > 0
+    ? getApps()[0]
+    : initializeApp({
+        credential: cert({
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+        }),
+      });
 
 const verifyToken = (req, res, next) => {
   const token = req?.cookies?.token;
